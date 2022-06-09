9d75a8fc4619010571236b5cf3bd1a3fbbba8293
Hsiao Yao <tomtogo1260@gmail.com>
2022-06-09 13:54:10 +0800

    using double quotes for string

--- src/ui.js
+++ src/ui.js
@@ -1,52 +1,52 @@
 const toggleButton = (className, callback) => {
     let button = document.querySelector(className);
-    button.addEventListener('click', (el) => {
-        if(button.classList.contains('active')) {
-            button.classList.remove('active');
+    button.addEventListener("click", (el) => {
+        if(button.classList.contains("active")) {
+            button.classList.remove("active");
             callback(true);
         } else {
-            button.classList.add('active');
+            button.classList.add("active");
             callback(false);
         }
     }, false);
 }
 
 const createPermaLink = (state) => {
-    let button = document.querySelector('.permalink');
+    let button = document.querySelector(".permalink");
 
     
-    button.addEventListener('click', () => {
+    button.addEventListener("click", () => {
         let urlSearchParams = new URLSearchParams(window.location.search);
-        urlSearchParams.set('code', encodeURI(state.code));
+        urlSearchParams.set("code", encodeURI(state.code));
         window.location.search = urlSearchParams.toString();
     }, false);
 }
 
 const showHideButtonInteraction = () => {
-    let codeContainer = document.querySelector('.code-container');
-    let logo = document.querySelector('.logo');
-    let permalink = document.querySelector('.permalink');
+    let codeContainer = document.querySelector(".code-container");
+    let logo = document.querySelector(".logo");
+    let permalink = document.querySelector(".permalink");
     let elements = [codeContainer, logo];
     
-    let showHideButton = document.querySelector('.show-hide-editor');
-    toggleButton('.show-hide-editor', (active) => {
+    let showHideButton = document.querySelector(".show-hide-editor");
+    toggleButton(".show-hide-editor", (active) => {
         if(active) {
-            showHideButton.innerHTML= 'Hide Code';
-            elements.forEach(el => el.style.display = 'block');
+            showHideButton.innerHTML= "Hide Code";
+            elements.forEach(el => el.style.display = "block");
         } else {
-            showHideButton.innerHTML= 'Show Code';
-            elements.forEach(el => el.style.display = 'none');
+            showHideButton.innerHTML= "Show Code";
+            elements.forEach(el => el.style.display = "none");
         }
     });
-    // showHideButton.addEventListener('click', (el) => {
-    //     if(showHideButton.classList.contains('show')) {
-    //         showHideButton.classList.remove('show');
-    //         showHideButton.innerHTML= '>';
-    //         codeContainer.style.display = 'block';
+    // showHideButton.addEventListener("click", (el) => {
+    //     if(showHideButton.classList.contains("show")) {
+    //         showHideButton.classList.remove("show");
+    //         showHideButton.innerHTML= ">";
+    //         codeContainer.style.display = "block";
     //     } else {
-    //         showHideButton.classList.add('show')
-    //         showHideButton.innerHTML= '^';
-    //         codeContainer.style.display = 'none';
+    //         showHideButton.classList.add("show")
+    //         showHideButton.innerHTML= "^";
+    //         codeContainer.style.display = "none";
     //     }
     // }, false);
 }
